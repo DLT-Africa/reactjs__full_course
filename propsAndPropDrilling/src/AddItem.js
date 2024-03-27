d4bd4deb6388db77b758362751c26abdf6f8b506
DLT-Africa <142330919+DLT-Africa@users.noreply.github.com>
2024-03-27 09:05:35 +0100

    folders added

--- propsAndPropDrilling/src/AddItem.js
+++ propsAndPropDrilling/src/AddItem.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import { FaPlus } from 'react-icons/fa6'
+import {useRef} from "react"
 
 const AddItem = ({newItem, setNewItem, handleSubmit}) => {
+    const inputRef = useRef();
   return (
    <form className='addForm' onSubmit={handleSubmit}>
-        <label htmlFor='addItem'>
-            Add Item
-        </label>
+        <label htmlFor='addItem'>Add Item</label>
 
         <input 
-        type="text"
         autoFocus
+        ref={inputRef}
+        type="text"
         id='addItem'
         placeholder='Add Item'
         required
@@ -21,7 +22,7 @@ const AddItem = ({newItem, setNewItem, handleSubmit}) => {
         <button
             type='submit'
             aria-label='Add Item'
-           
+            onClick={(e) => inputRef.current.focus()}
         >
             <FaPlus/>
         </button>
--- propsAndPropDrilling/src/App.js
+++ propsAndPropDrilling/src/App.js
@@ -1,84 +1,70 @@
-import {useState} from 'react'
-// import logo from "./logo.svg";
+import { useState } from "react";
 import "./App.css";
 import Header from "./Header";
 import Content from "./Content";
 import Footer from "./Footer";
-import AddItem from './AddItem';
-// [].length = the amount of items in that array
+import AddItem from "./AddItem";
+import Searchitem from "./Searchitem";
 
 function App() {
-  // const name = "Hayzed";
-  const [items, setItems] = useState([
-    {
-      id: 1,
-      checked: false,
-      item: "A bag of Garri",
-    },
-    {
-      id: 2,
-      checked: false,
-      item: "Rice",
-    },
-    {
-      id: 3,
-      checked: false,
-      item: "Semo",
-    },
-    {
-      id: 4,
-      checked: false,
-      item: "ponmo",
-    },
-  ]);
-  const [newItem, setNewItem] = useState('')
+  const [items, setItems] = useState(
+    JSON.parse(localStorage.getItem("shoppinglist"))
+  );
+
+  const [newItem, setNewItem] = useState("");
+  const [search, setSearch] = useState("");
+
+  const setAndSaveItems = (newItems) => {
+    setItems(newItems);
+    localStorage.setItem("shoppinglist", JSON.stringify(newItems));
+  };
 
   const addItem = (item) => {
     const id = items.length ? items[items.length - 1].id + 1 : 1;
-    const myNewItem = {id, checked: false, item}
-    const listItems = [...items, myNewItem]
-    setItems(listItems);
-    localStorage.setItem("shoppinglist", JSON.stringify(listItems));
-  }
+    const myNewItem = { id, checked: false, item };
+    const listItems = [...items, myNewItem];
+    setAndSaveItems(listItems);
+  };
 
   const handleCheck = (value) => {
-    // console.log(`key: ${id}`)
     const listItems = items.map((item) =>
       item.id === value ? { ...item, checked: !item.checked } : item
     );
-    setItems(listItems);
-    localStorage.setItem("shoppinglist", JSON.stringify(listItems));
+    setAndSaveItems(listItems);
   };
 
   const handleDelete = (id) => {
     const listItems = items.filter((item) => item.id !== id);
-    setItems(listItems);
-  }
+    setAndSaveItems(listItems);
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if(!newItem) return;
-    addItem(newItem)
-    setNewItem('')
-  }
- 
+    if (!newItem) return;
+    addItem(newItem);
+    setNewItem("");
+  };
 
   return (
     <div className="App">
-      <Header  title = "Welcome to props"/>
+      <Header title="Welcome to props" />
+
+      <Searchitem search={search} setSearch={setSearch} />
+
       <AddItem
         newItem={newItem}
         setNewItem={setNewItem}
         handleSubmit={handleSubmit}
       />
 
-      <Content 
-        items={items}
+      <Content
+        items={items.filter((item) =>
+          item.item.toLowerCase().includes(search.toLocaleLowerCase())
+        )}
         handleCheck={handleCheck}
         handleDelete={handleDelete}
       />
-      <Footer length={items.length}/>
-      
+      <Footer length={items.length} />
     </div>
   );
 }
